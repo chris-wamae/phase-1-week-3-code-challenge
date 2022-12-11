cec7485a67f5fb7d500299e153928723b1b58825
chris-wamae <chriswamae123@gmail.com>
2022-12-11 21:20:32 +0300

    fetched films data from db.json

---
+++ index.js
@@ -0,0 +1,7 @@
+const baseUrl = "http://localhost:3000/films";
+fetch(baseUrl)
+function fetchMovies(){
+    fetch(baseUrl)
+        .then((response) => response.json())
+        .then((data) =>{
+            data})
