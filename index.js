3ea9014220ce23f38f80cd8f29e0d0dc85a6faf7
chris-wamae <chriswamae123@gmail.com>
2022-12-11 21:26:28 +0300

    Added film title to click event listener

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ function fetchMovies(){
               let li = document.createElement("li");
               li.textContent = films.title;
               li.addEventListener("click",
-              (e)=>{
+              (e)=>{let buttonContent =
+                document.querySelector("button#buy-ticket")
+                buttonContent.textContent = "Buy Tickets"
+                    let title =
+                 document.getElementById("movie-title");
+                    title.textContent =
+                films.title;
